5cb598bb7df090dae38db5873ae05598218653e3
MasterChef444 <justinsoto2424@gmail.com>
2024-02-03 15:17:54 -0500

    rule enforcing
    model will now guard against moves on non-empty cells

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 from model.game import Game
 
 game = Game()
-game.make_move(0, 0)
-game.make_move(2, 3)
-game.make_move(3, 4)
-game.make_move(7, 7)
+game.make_move(3, 3)
 print(game)
-
--- model/game.py
+++ model/game.py
@@ -15,6 +15,18 @@ class Game:
         return view.__str__()
 
     def make_move(self, row, col):
+        # Check if space is empty 
+        if not self.board.is_cell_empty(row, col):
+            print("Taken cell. Try again.\n")
+            
+        # Check if move is legal 
         self.board.make_move(row, col, self.current_player)
+
+    # Gives turn to the opposite player
+    def swap_turns(self) -> None:
+        if self.current_player == self.player1:
+            self.current_player = self.player2
+        else:
+            self.current_player = self.player1
 
     
