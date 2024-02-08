be199307485a8fce92672e893809e29a61f81203
catherinec57 <119818203+catherinec57@users.noreply.github.com>
2024-02-08 17:51:13 -0500

    Update console_game_view.py

--- view/console_game_view.py
+++ view/console_game_view.py
@@ -1,5 +1,35 @@
 from view.console_board_view import ConsoleBoardView
+from model.player import Player
+from model.player_color import color_to_symbol
+from model.game import Game
 
 class ConsoleGameView(ConsoleBoardView):
     def __init__(self):
+        pass
+
+    def display_current_player(self):
+        pass
+
+    def display_illegal_move_message(self, row, col):
+        pass
+
+    def display_winner(self):
+        pass
+
+    def display_draw_message(self):
+        pass
+
+    def display_score(self, player):
+        pass
+
+    def display_legal_moves(self, player):
+        pass
+
+    def display_legal_moves(self):
+        pass
+
+    def display_final_scoreboard(self):
+        pass
+
+    def get_move(self):
         pass
