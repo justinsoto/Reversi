d11a24ef17ecb226357b71cc30d3f97d90954107
sotojustin <justinsoto2424@gmail.com>
2024-02-08 08:58:29 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from model.game import Game
 from view.board_view import BoardView
 from controller.controller import GameController
+
 game = Game(4)
 view = BoardView(game)
 controller = GameController(game,view)
