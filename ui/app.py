460169e0c5a020df32f59b6eca6bac1ac9593f31
melarb <muhammad.elarbi13@gmail.com>
2024-03-11 16:12:09 -0400

    Fix API

--- ui/app.py
+++ ui/app.py
@@ -1,13 +1,15 @@
 # app.py (Flask App to serve as backend)
 from flask import Flask, jsonify
-from flask_cors import CORS
+#from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
 
-@app.route('/api/data')
-def get_data():
+@app.route('/hello')
+def hello():
+    return 'Hello, World!'
+'''def get_data():
     return jsonify({'message': 'Hello from Flask!'})
-
+'''
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
