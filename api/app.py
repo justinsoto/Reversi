9aebc1fe92f60a841a7fca2a3caa1fd2596ff486
sotojustin <justinsoto2424@gmail.com>
2024-03-12 14:00:38 -0400

    Working API: React-Python

--- api/app.py
+++ api/app.py
@@ -1,15 +1,15 @@
 # app.py (Flask App to serve as backend)
 from flask import Flask, jsonify
-#from flask_cors import CORS
 
 app = Flask(__name__)
-#CORS(app)
+
+@app.route('/')
+def main_page():
+    return "Main Page"
 
 @app.route('/hello')
 def hello():
     return 'Hello, World!'
-'''def get_data():
-    return jsonify({'message': 'Hello from Flask!'})
-'''
+
 if __name__ == '__main__':
     app.run()
--- view/my-react-reversi/src/ReversiApp.jsx
+++ view/my-react-reversi/src/ReversiApp.jsx
@@ -1,17 +1,22 @@
 import Title from "./Title";
 import Game from "./Game";
+import React, { Component } from "react";
+import axios from "axios";
 
-function ReversiApp() {
+export default function ReversiApp() {
 
     document.title = "Reversi";
+    fetchAPI();
+
+    function fetchAPI() {
+        axios.get('http://127.0.0.1:5000/hello')
+            .then(response => console.log(response.data))
+    }
 
     return (
         <>
             <Title />
-            <Game size={8}/>
+            <Game size={8} />
         </>
-
     );
 }
-
-export default ReversiApp
