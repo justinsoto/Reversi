9fcae860355496a0d055bde5f27a78f01c8e137f
catherinec57 <119818203+catherinec57@users.noreply.github.com>
2024-04-04 18:04:00 -0400

    Database Proxy
    Validates Input and Prevents SQL Injections

--- database/db_facade.py
+++ database/db_facade.py
@@ -3,10 +3,9 @@ from view.console_items.console_game_view import ConsoleGameView
 from controller.controller import GameController
 from mysql.connector import connect, Error
 from getpass import getpass
-from database.db_management.user_manager import UserManager
+from database.user_manager_proxy import UserManagerProxy
 from database.db_management.games_manager import GamesManager
 from database.db_management.ratings_manager import RatingsManager
-from database.db_management.user_manager_decorator import LoginDecorator
 
 class database:
     def __init__(self):
@@ -14,25 +13,25 @@ class database:
         self.connection = connection
         self.ratings_manager = RatingsManager(connection)
         self.games_manager = GamesManager(connection)
+        self.user_manager_proxy = UserManagerProxy(connection)
         self.player1_id = None
         self.player2_id = None
         self.game_id = None
-        self.login_manager = LoginDecorator(connection, UserManager(connection))
 
     def create_users(self):
         print("Player 1: \n")
-        self.user_manager.create_user(input("Please Enter Your Username: "),getpass("Please Enter Your Password: "))
-        self.player1_id = self.user_manager.get_current_user().get_user_id()
+        self.user_manager_proxy.create_user(input("Please Enter Your Username: "),getpass("Please Enter Your Password: "))
+        self.player1_id = self.user_manager_proxy.get_current_user().get_user_id()
         print("Player 2: \n")
-        self.user_manager.create_user(input("Please Enter Your Username: "),getpass("Please Enter Your Password: "))
-        self.player2_id = self.user_manager.get_current_user().get_user_id()
+        self.user_manager_proxy.create_user(input("Please Enter Your Username: "),getpass("Please Enter Your Password: "))
+        self.player2_id = self.user_manager_proxy.get_current_user().get_user_id()
         self.game_id = self.games_manager.create_game(self.player1_id, self.player2_id)
 
     def login_users(self):
         p = False
         while p == False:
             print("Player 1: ")
-            p = self.login_manager.login(input("Please enter your username: "), getpass("Please enter your password: "))
+            p = self.user_manager_proxy.login(input("Please enter your username: "), getpass("Please enter your password: "))
             if p == False:
                 print("Invalid Username/Password Combo. Please try again or create new user")
             else:
@@ -40,7 +39,7 @@ class database:
         q = False
         while q == False:
             print("Player 2: ")
-            q = self.login_manager.login(input("Please enter your username: "), getpass("Please enter your password: "))
+            q = self.user_manager_proxy.login(input("Please enter your username: "), getpass("Please enter your password: "))
             if q == False:
                 print("Invalid Username/Password Combo. Please try again or create new user")
             else:
---
+++ database/user_manager_proxy.py
@@ -0,0 +1,58 @@
+from database.db_management.user_manager import UserManager
+import re
+
+class UserManagerProxy:
+    def __init__(self, connection):
+        self.user_manager = UserManager(connection)
+
+    def validate_input(self, input_string):
+        pattern = r'^[a-zA-Z0-9_!&$?-]+$'
+        if re.match(pattern, input_string):
+            return True
+        else:
+            return False
+        
+    def check_sql_injection(self, input_string):
+        sql_injections = ["'", '"', ";", "DROP TABLE", "SELECT * FROM"]
+        for injection in sql_injections:
+            if injection in input_string:
+                return False 
+        return True
+    
+    def create_user(self, username, password):
+        if self.validate_input(username) and self.validate_input(password)\
+        and self.check_sql_injection(username)\
+        and self.check_sql_injection(password):
+            self.user_manager.create_user(username, password)
+        else: 
+            return "Invalid input. Please try again."
+        
+    def login(self, username, password):
+        if self.validate_input(username) and self.validate_input(password)\
+        and self.check_sql_injection(username)\
+        and self.check_sql_injection(password):
+            return self.user_manager.login(username, password)
+        else:
+            return "Invalid input. Please try again."
+    
+    def get_current_user(self):
+        return self.user_manager.get_current_user()
+    
+    def get_user_by_id(self, user_id):
+        if user_id.isdigit():
+            return self.user_manager.get_user_by_id(user_id)
+        else:   
+            return "Invalid user ID format."
+    
+    def get_user_by_usernam(self, username):
+        if self.validate_input(username) and self.check_sql_injection(username):
+            return self.user_manager.get_user_by_username(username)
+        else:
+            return "Invalid username format."
+        
+    def delete_user(self, username):
+        if self.validate_input(username) and self.check_sql_injection(username):
+            return self.user_manager.delete_user(username)
+        else:
+            return "Invalid username format."
+        
