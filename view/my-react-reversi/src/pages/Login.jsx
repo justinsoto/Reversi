451fe501d129144e100396d0c63642b40c265c7c
sotojustin <justinsoto2424@gmail.com>
2024-04-16 12:44:47 -0400

    Update Login.jsx

--- view/my-react-reversi/src/pages/Login.jsx
+++ view/my-react-reversi/src/pages/Login.jsx
@@ -6,39 +6,48 @@ export default function Login() {
 
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
+    const [message, setMessage] = useState("")
     const navigate = useNavigate()
 
     function login() {
         if (username === "" | password === "") {
-            return
+            setMessage("Fields cannot be empty")
         }
-        
-        baseURL.get(`/login/${username}/${password}`)
-        .then(res => {
-            console.log(username)
-            console.log(res.data)
-            navigate('/game')
-        })
+        else {
+            baseURL.get(`/login/${username}/${password}`)
+                .then(res => {
+                    console.log(username)
+                    console.log(res.data)
+                    navigate('/game')
+                })
+        }
+
     }
 
     function register() {
-        baseURL.get(`/register/${username}/${password}`)
-        .then(res => {
-            console.log(username)
-            console.log(res.data)
-            navigate('/game')
-        })
-        
+        if (username === "" | password === "") {
+            setMessage("Fields cannot be empty")
+        }
+        else {
+            baseURL.get(`/register/${username}/${password}`)
+                .then(res => {
+                    console.log(username)
+                    console.log(res.data)
+                    navigate('/game')
+                })
+        }
     }
 
     return (
         <div className="login-container">
             <div className="user-form">
                 <label>Username:</label>
-                <input type="text" onChange={(e) => {setUsername(e.target.value)}}/><br />
+                <input type="text" onChange={(e) => { setUsername(e.target.value) }} /><br />
                 <label>Password:</label>
-                <input type="text" onChange={(e) => {setPassword(e.target.value)}}/><br />
+                <input type="text" onChange={(e) => { setPassword(e.target.value) }} /><br />
             </div>
+
+            <p>{message}</p>
 
             <Link onClick={login}>
                 <button>Login</button>
